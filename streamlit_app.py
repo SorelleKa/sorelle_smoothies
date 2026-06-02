889ff777c34c21b2c8b2d4076da0fb4b218c9811
SorelleKa <113608753+SorelleKa@users.noreply.github.com>
2026-06-02 16:52:26 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,8 @@ from snowflake.snowpark.functions import col,when_matched
 #New section to display smoothiefroot nutrition information
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
 
 # Write directly to the app
